569242d51ad9b156570a38beaefc485584e7b80a
SoNgSeUNgMiN96 <tmd9544@gmail.com>
2022-06-22 02:16:46 +0900

    AOP 적용
    작성일 : 2022-06-22

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,5 +1,6 @@
 package hello.hellospring;
 
+import hello.hellospring.aop.TimeTraceAop;
 import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +26,10 @@ public class SpringConfig {
         return new MemberService(memberRepository);
     }
 
+   /* @Bean
+    public TimeTraceAop timeTraceAop(){
+        return new TimeTraceAop();
+    }*/
 
 
 //    private DataSource dataSource;
